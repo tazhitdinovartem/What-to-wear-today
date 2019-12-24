1b9e8dba80391f2924fa0eacc6f6e649207ecffd
artem <tazhitdinov.artem@gmail.com>
2019-12-24 23:26:29 +0500

    fix

--- main.rb
+++ main.rb
@@ -4,7 +4,8 @@ require_relative "lib/clothes_collection"
 files_path = File.dirname(__FILE__) + '/data/*.txt'
 files = Dir[files_path]
 
-collection = ClothesCollection.make_collection(files)
+all_types_collection = ClothesCollection.make_collection(files)
+collection = ClothesCollection.new(all_types_collection)
 
 puts "Сколько градусов за окном? (можно с минусом)"
 user_input = gets.to_i
