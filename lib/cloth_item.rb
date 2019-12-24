ee3aa89f4363c6ea77b6dc8f13c30ee314a938f4
tazhitdinov.artem <tazhitdinov.artem@gmail.com>
2019-12-24 17:57:08 +0500

    Ranges added

--- lib/cloth_item.rb
+++ lib/cloth_item.rb
@@ -3,7 +3,12 @@ class ClothItem
   def initialize(name, type, temp_range)
     @name = name
     @type = type
-    @temp_range = temp_range  
+    @temp_range = get_range(temp_range)  
+  end
+
+  def get_range(temp_range)
+    ranges = temp_range.scan(/-?\d+/)
+    Range.new(ranges[0], ranges[1])
   end
 
   def suitable?(temp)
