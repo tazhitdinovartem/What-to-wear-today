f43445fccd7b231e16683ee4120332a734336f3e
artem <tazhitdinov.artem@gmail.com>
2019-12-25 10:59:04 +0500

    fixed

--- lib/clothes_collection.rb
+++ lib/clothes_collection.rb
@@ -9,7 +9,8 @@ class ClothesCollection
       ClothItem.new(cloth_files[0], cloth_files[1], cloth_files[2])
     end
   end
-
+  #тут будет метод, который забирает из suitable_collection несколько вещей одного типа
+  #возвращает рандомную вещь этого же типа в коллекцию
   def get_types_of_cloths
     @collection
       .map { |item| item.type }
@@ -17,10 +18,11 @@ class ClothesCollection
   end
 
   def get_same_type_cloths
-
+    @suitable_collection.group_by{|item| item.type}
   end
 
   def generate(temp)
-    @collection.select { |item| item.suitable?(temp)}
+    @suitable_collection = 
+      @collection.select { |item| item.suitable?(temp)}
   end
 end
